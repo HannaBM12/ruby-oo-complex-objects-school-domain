3fdcb543f084e6a6a8282df6f79cc9b9fd638175
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-05 16:10:52 -0500

    commit tree 701c4c4987018af1476796ec35662a22181615e5

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,64 @@
-# code here!
-require 'pry'
+
 class School
-    attr_reader :roster
-    def initialize (name, roster = {})
+    attr_reader :name, :roster
+    def initialize(name, roster = {})
         @name = name
-        @roster = {}
+        @roster = roster
     end
 
     def add_student(name, grade)
-        if @roster.has_key?(grade)    
-                @roster[grade] << name
+        if @roster.has_key?(grade)
+            @roster[grade] << name
         else
             @roster[grade] = []
             @roster[grade] << name
-            
         end
-        
-    end 
+    end
 
     def grade(grade)
-        @roster[grade]
+       @roster[grade]
     end
 
     def sort
-        @roster.collect{|key, value|
-            [key, value.sort]}.to_h
+        @roster.map{|k, v| [k, v.sort]}.to_h
     end
 
 end
+
+
+
+
+
+
+
+
+# code here!
+# require 'pry'
+# class School
+#     attr_reader :roster
+#     def initialize (name, roster = {})
+#         @name = name
+#         @roster = {}
+#     end
+
+#     def add_student(name, grade)
+#         if @roster.has_key?(grade)    
+#                 @roster[grade] << name
+#         else
+#             @roster[grade] = []
+#             @roster[grade] << name
+            
+#         end
+        
+#     end 
+
+#     def grade(grade)
+#         @roster[grade]
+#     end
+
+#     def sort
+#         @roster.collect{|key, value|
+#             [key, value.sort]}.to_h
+#     end
+
+# end
